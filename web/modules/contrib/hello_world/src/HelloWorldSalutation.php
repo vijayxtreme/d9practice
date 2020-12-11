b1237707c3a1f4cc1334d659505c5f96584f617c
Vijay Menon <vijaymenonx@gmail.com>
2020-12-11 15:33:26 -0800

    Finished forms

--- web/modules/contrib/hello_world/src/HelloWorldSalutation.php
+++ web/modules/contrib/hello_world/src/HelloWorldSalutation.php
@@ -3,12 +3,28 @@
 namespace Drupal\hello_world;
 
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * Prepares the salutation to the world.
  */
 
 class HelloWorldSalutation {
+    /**
+     * @var \Drupal\Core\Config\ConfigFactoryInterface
+     */
+    protected $configFactory;
+
+    /**
+     * HelloWorldSalutation constructor
+     * 
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+     */
+    public function __construct(ConfigFactoryInterface $config_factory){
+        $this->configFactory = $config_factory;
+    }
+
+
     /** StringTranslationTrait must be used after imported with class */
     use StringTranslationTrait;
 
@@ -16,6 +32,13 @@ class HelloWorldSalutation {
      * Returns the salutation
      */
     public function getSalutation(){
+        $config = $this->configFactory->get('hello_world.custom_salutation');
+        $salutation = $config->get('salutation');
+
+        if($salutation != ""){
+            return $salutation;
+        }
+
         /** \DateTime() is a native function */
         $time = new \DateTime();
         if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12){
